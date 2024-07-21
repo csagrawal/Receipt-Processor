e161f0933408694fa6733a8ea1244ed7ce6b96ee
csagrawal <agrawalchanchal1202@gmail.com>
2024-07-20 23:02:55 -0400

    environment and server setup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"receipt-processor/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func testServer(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello, World!",
+	})
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/", testServer)
+	router.Run()
 }
